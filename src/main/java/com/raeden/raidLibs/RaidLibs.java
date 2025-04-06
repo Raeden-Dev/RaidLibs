5fbef8439df70a454f01fd70cfb988106c3557e3
Sadman Sakib <raider2891@gmail.com>
2025-04-06 07:05:25 +0600

    Initial commit

---
+++ src/main/java/com/raeden/raidLibs/RaidLibs.java
@@ -0,0 +1,19 @@
+package com.raeden.raidLibs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class RaidLibs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
